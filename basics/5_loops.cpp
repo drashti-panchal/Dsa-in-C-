8881e7e7b04f4e850d12106cc660f95a2bd1bbbb
drashti-panchal <drashtipanchal112@gmail.com>
2024-07-18 19:44:00 +0530

    For,while,do-while

---
+++ basics/5_loops.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//FOR LOOP
+	for(int i =5 ;i>0;i--){
+		cout<<i<<endl;
+	}
+
+	//While Loop
+
+	int j = 5;
+	while(j>0){
+		cout<<j<<endl;
+		j--;
+	}
+
+	//Do while loop
+	int i=10;
+	do{
+		cout<<i<<endl;
+		i++;
+
+	}while(i<0);
+
+
+
+}
