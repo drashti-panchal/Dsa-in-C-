5dca702551efd7a99050a3dfaf3534c415d5531b
drashti-panchal <drashtipanchal112@gmail.com>
2024-07-19 11:56:47 +0530

    Pattern 1

---
+++ 2_Patterns/1.cpp
@@ -0,0 +1,28 @@
+
+
+//  * * * *
+//  * * * *
+//  * * * *
+//  * * * *
+
+
+// RULES FOR MAKING ANY PATTERN
+// 1) Check the rows
+// 2) Check and columns and make relationship with the rows
+// 3) Print in the inner loop
+// 4) Check symmetry (optional)
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
