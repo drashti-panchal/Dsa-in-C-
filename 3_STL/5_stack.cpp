5028960431533bfd93a82b9fb7f000f2c61bd7dc
drashti-panchal <drashtipanchal112@gmail.com>
2024-07-21 10:04:08 +0530

    Stack

---
+++ 3_STL/5_stack.cpp
@@ -0,0 +1,24 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+	// Stack is also a container which uses LIFO property.
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.emplace(4);
+
+	cout<<s.top()<<endl; //4
+
+	cout<<s.empty()<<endl; //false
+
+	s.pop();
+
+	cout<<s.top()<<endl; //3
+
+	stack<int> st1,st2;
+	st1.swap(st2);
+}
